fbaba65ca1340c2e05ee8c47c3086e0981a21718
Allister <allisterkmoon@gmail.com>
2019-02-16 22:41:03 -0800

    Fill in hints for columns and rows

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
   <div id="app">
     <h1>Sudoko Solver</h1>
+    <div>
+      <div>Show possible answers in cells</div>
+
+      <label for="showPossibleAnswers-false">No</label>
+      <input type="radio" name="showPossibleAnswers" id="showPossibleAnswers-true"
+        :value="false" v-model.lazy="showPossibleAnswers" />
+      <label for="showPossibleAnswers-true">Yes</label>
+      <input type="radio" name="showPossibleAnswers" id="showPossibleAnswers-false"
+        :value="true" v-model.lazy="showPossibleAnswers" />
+    </div>
     <div class="grid">
-      <div
-        v-for="i in 81" :key="'cell-' + i"
+      <div v-for="i in 81" :key="'cell-' + i"
         class="cell" :class="{ selected: (i - 1) === activeCell }"
         @click="setCellActive(i - 1)">
-        {{ cells[i - 1] || '' }}
+        <span v-if="cells[i - 1]">{{ cells[i - 1] }}</span>
+        <div v-if="showPossibleAnswers && !cells[i - 1]"
+          class="cell-answers-wrapper">
+          <span>{{ possibleAnswers[i - 1][1] ? 1 : '' }}</span>
+          <span>{{ possibleAnswers[i - 1][2] ? 2 : '' }}</span>
+          <span>{{ possibleAnswers[i - 1][3] ? 3 : '' }}</span>
+          <span>{{ possibleAnswers[i - 1][4] ? 4 : '' }}</span>
+          <span>{{ possibleAnswers[i - 1][5] ? 5 : '' }}</span>
+          <span>{{ possibleAnswers[i - 1][6] ? 6 : '' }}</span>
+          <span>{{ possibleAnswers[i - 1][7] ? 7 : '' }}</span>
+          <span>{{ possibleAnswers[i - 1][8] ? 8 : '' }}</span>
+          <span>{{ possibleAnswers[i - 1][9] ? 9 : '' }}</span>
+        </div>
       </div>
 
       <div class="grid-divider col-1"></div>
@@ -18,20 +39,26 @@
 </template>
 
 <script>
+import getPossibleAnswers from '@/lib/getPossibleAnswers'
+
 export default {
   name: 'App',
   data () {
     return {
       activeCell: null,
-      cells: Array.apply(null, Array(81)).map(() => {})
+      cells: Array.apply(null, Array(81)).map(() => {}),
+      possibleAnswers: null,
+      showPossibleAnswers: false
     }
   },
   created () {
+    this.updatePossibleAnswers()
     window.addEventListener('keyup', e => {
-      if (this.activeCell) {
+      if (!isNaN(this.activeCell) && this.activeCell !== null) {
         if (!isNaN(e.key)) {
           this.cells.splice(this.activeCell, 1, Number(e.key))
           this.activeCell = null
+          this.updatePossibleAnswers()
         }
       }
     })
@@ -45,6 +72,10 @@ export default {
       } else {
         this.activeCell = i
       }
+    },
+    updatePossibleAnswers: function () {
+      if (!this.possibleAnswers) this.possibleAnswers = []
+      this.possibleAnswers.splice(0, 81, ...getPossibleAnswers(this.cells))
     }
   }
 }
@@ -87,6 +118,24 @@ h1 {
 }
 .cell.selected {
   background-color: yellow;
+}
+
+.cell-answers-wrapper {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+
+  font-size: 0.7rem;
+  font-weight: normal;
+  line-height: 100%;
+
+  height: 100%;
+  width: 100%;
+}
+.cell-answers-wrapper span {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .grid-divider {
--- src/lib/getPossibleAnswers.js
+++ src/lib/getPossibleAnswers.js
@@ -1,3 +1,59 @@
 export default (cells) => {
-  return cells
+  if (!cells || !Array.isArray(cells) || cells.length !== 81) {
+    return Array.apply(null, Array(87)).map(() => {})
+  }
+
+  return cells.map((cell, i) => {
+    if (cell) return null
+    return {
+      1: !isNumberInRow(1, i, cells) && !isNumberInColumn(1, i, cells) && !isNumberInBlock(1, i, cells),
+      2: !isNumberInRow(2, i, cells) && !isNumberInColumn(2, i, cells) && !isNumberInBlock(2, i, cells),
+      3: !isNumberInRow(3, i, cells) && !isNumberInColumn(3, i, cells) && !isNumberInBlock(3, i, cells),
+      4: !isNumberInRow(4, i, cells) && !isNumberInColumn(4, i, cells) && !isNumberInBlock(4, i, cells),
+      5: !isNumberInRow(5, i, cells) && !isNumberInColumn(5, i, cells) && !isNumberInBlock(5, i, cells),
+      6: !isNumberInRow(6, i, cells) && !isNumberInColumn(6, i, cells) && !isNumberInBlock(6, i, cells),
+      7: !isNumberInRow(7, i, cells) && !isNumberInColumn(7, i, cells) && !isNumberInBlock(7, i, cells),
+      8: !isNumberInRow(8, i, cells) && !isNumberInColumn(8, i, cells) && !isNumberInBlock(8, i, cells),
+      9: !isNumberInRow(9, i, cells) && !isNumberInColumn(9, i, cells) && !isNumberInBlock(9, i, cells)
+    }
+  })
+}
+
+const getRowOfCell = i => {
+  return Math.floor(i / 9)
+}
+const getColumnOfCell = i => {
+  return i % 9
+}
+const getBlockOfCell = i => {
+  const row = getRowOfCell(i)
+  const column = getColumnOfCell(i)
+
+  return (Math.floor(row / 3) * 3) + Math.floor(column / 3)
+}
+
+const isNumberInRow = (num, i, cells) => {
+  const row = getRowOfCell(i)
+  return [0, 1, 2, 3, 4, 5, 6, 7, 8].some(c => {
+    return cells[row * 9 + c] === num
+  })
+}
+
+const isNumberInColumn = (num, i, cells) => {
+  const column = getColumnOfCell(i)
+
+  return [0, 1, 2, 3, 4, 5, 6, 7, 8].some(c => {
+    return cells[c * 9 + column] === num
+  })
+}
+
+const isNumberInBlock = (num, i, cells) => {
+  return false
+  // const block = getBlockOfCell(i)
+  // if (block === 4) console.log('I am a 4 boi', i, getRowOfCell(i), getColumnOfCell(i))
+  //
+  // if (i === 19) console.log('WOO!', num, i, block)
+  // return [0, 1, 2, 9, 10, 11, 18, 19, 20].some(c => {
+  //   return cells[(block * 3) + (Math.floor(block / 3) * 9) + c] === num
+  // })
 }
