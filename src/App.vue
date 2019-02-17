14806d97d2512c682ca202957251dafd61b59b48
Allister <allisterkmoon@gmail.com>
2019-02-16 19:20:20 -0800

    Add more distinctive grid blocks

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
   <div id="app">
     <h1>Sudoko Solver</h1>
     <div class="grid">
-      <div class="block" v-for="i_block in 9">
-        <div class="cell" v-for="i_cell in 9"></div>
-      </div>
+      <div class="cell" v-for="i in 81"></div>
+
+      <div class="grid-divider col-1"></div>
+      <div class="grid-divider col-2"></div>
+      <div class="grid-divider row-1"></div>
+      <div class="grid-divider row-2"></div>
     </div>
   </div>
 </template>
@@ -21,32 +24,50 @@ h1 {
 }
 
 .grid {
+  background-color: black;
   box-shadow: 6px 6px 10px 6px black;
+  border: 4px solid black;
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(3, 1fr);
+  grid-gap: 1px;
+  grid-template-columns: repeat(9, 1fr);
+  grid-template-rows: repeat(9, 1fr);
   height: 30rem;
-  margin: 5rem auto;
+  margin: 4rem auto;
+  position: relative;
   width: 30rem;
-}
-
-.block {
-  border: 3px solid black;
-  border-bottom-width: 4px;
-  border-right-width: 4px;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(3, 1fr);
-  height: 100%;
-  width: 100%;
 }
 
 .cell {
   background-color: #EEEEEE;
-  border: 1px solid rgb(50, 50, 50);
   height: 100%;
-  margin: 0;
   width: 100%;
+}
+
+.grid-divider {
+  background-color: black;
+  position: absolute;
+}
+.grid-divider[class *= 'col-'] {
+  top: 0;
+  bottom: 0;
+  width: 3px;
+}
+.grid-divider[class *= 'row-'] {
+  left: 0;
+  right: 0;
+  height: 3px;
+}
+.grid-divider.col-1 {
+  left: 33%;
+}
+.grid-divider.col-2 {
+  right: 33%;
+}
+.grid-divider.row-1 {
+  top: 33%;
+}
+.grid-divider.row-2 {
+  bottom: 33%;
 }
 
 </style>
